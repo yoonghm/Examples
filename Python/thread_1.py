531d5d59b034d9013d91358ec8f003f125507105
YoongHM <yoonghm@users.noreply.github.com>
2018-03-07 09:24:15 +0800

    Create thread_1.py

---
+++ Python/thread_1.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin python3
+
+import threading
+
+def worker():
+  print('Worker')
+  return
+
+threads = []
+for i in range(5):
+  t = threading.Thread(target = worker)
+  threads.append(t)
+  t.start()
