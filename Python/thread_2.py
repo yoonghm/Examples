578d2ab1319d8f23f654836e8e7a47977a15a4f1
YoongHM <yoonghm@users.noreply.github.com>
2018-03-07 09:50:00 +0800

    Create thread_2.py

---
+++ Python/thread_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import threading
+import time
+
+# num is positional args, sleep is keyword argument
+def worker(num, sleep=2):
+  print('Worker: ', num)
+  time.sleep(sleep)
+  return
+
+threads = []
+for i in range(5):
+  t = threading.Thread(target=worker, \
+                       args=(i,), \
+                       kwargs={'sleep':5}
+                      )
+  threads.append(t)
+  t.start()
+
+print('Waiting for child thread to return...')
