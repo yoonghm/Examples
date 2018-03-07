da686ee6e0f1520f9f197d82369ef27fa350046b
YoongHM <yoonghm@users.noreply.github.com>
2018-03-07 10:22:14 +0800

    Create logging_1.py

---
+++ Python/logging_1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import logging
+
+# The default logging level s WARNING
+logging.debug   ('This is programmer only' ) # Will not be printed out
+logging.info    ('This is for information' ) # Will not be printed out
+logging.warning ('This is a warning!'      ) # Will     be printed out
+logging.error   ('This is a software error') # Will     be printed out
+logging.critical('This is a critical error') # Will     be printed out
