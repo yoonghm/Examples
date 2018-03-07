69d74d3cfd46899f818657275f724457e93f4728
YoongHM <yoonghm@users.noreply.github.com>
2018-03-07 18:00:08 +0800

    Create types.py

---
+++ Python/types.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+a = (1)
+print('a = (1)')
+print('type(a) =', type(a), '\n')
+
+a = (1,)
+print('a = (1,)')
+print('type(a) =', type(a), '\n')
+
+a = [1]
+print('a = [1]')
+print('type(a) =', type(a), '\n')
+
+a = {'name':'Yoonghm', 'country':'Singapore'}
+print("a = {'name':'Yoonghm', 'country':'Singapore'}")
+print('type(a) =', type(a), '\n')
