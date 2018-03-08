e8c6f22bb2791a6d0d076ac80fabc32563103592
YoongHM <yoonghm@users.noreply.github.com>
2018-03-09 00:47:46 +0800

    Update format_print.py

--- Python/format_print.py
+++ Python/format_print.py
@@ -25,3 +25,28 @@ print("%o" % 100)
 print("%#o" % 100)
 print("%+2d" % 100)
 print("%+8d" % 100)
+print()
+
+str = '1th => {0}, 2th => {1}'.format(10, 99);            print(str)
+str = '1th => {1}, 2th => {0}'.format(10, 99);            print(str)
+str = '1th => {0:3.2f}, 2th => {1:3.2f}'.format(10, 99);  print(str)
+str = '1th => {1:5.2E}, 2th => {0:5.2E}'.format(10, 99);  print(str)
+print()
+
+str = '{0:<20s}: ${1:6.2f}'.format('Eggs',   0.20);       print(str)
+str = '{0:<20s}: ${1:6.2f}'.format('Mutton', 18.50);      print(str)
+print()
+
+str = '{0:>20s}: ${1:<6.2f}'.format('Eggs',   0.20);      print(str)
+str = '{0:>20s}: ${1:<6.2f}'.format('Mutton', 18.50);     print(str)
+print()
+
+str = '{0:>20s}: ${1:>12,}'.format('Watch',   360);       print(str)
+str = '{0:>20s}: ${1:>12,}'.format('House', 123456789);   print(str)
+print()
+
+str = '{0:>20s}: ${1:<12,}'.format('Watch',   360);       print(str)
+str = '{0:>20s}: ${1:<12,}'.format('House', 123456789);   print(str)
+print()
+
+print('This is {grade}.'.format(grade='good'))
