1bea58343b34fd7052133bec59256d1ff590a9c3
YoongHM <yoonghm@users.noreply.github.com>
2018-03-08 23:39:17 +0800

    Update types.py

--- Python/types.py
+++ Python/types.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python3
 
-a = (1)
-print('a = (1)')
-print('type(a) =', type(a), '\n')
+from sys import getsizeof, float_info
 
-a = (1,)
-print('a = (1,)')
-print('type(a) =', type(a), '\n')
+def show(var):
+   print('a =', var)
+   print('    is', type(var), 'and occupies', getsizeof(var), '\n')
 
-a = [1]
-print('a = [1]')
-print('type(a) =', type(a), '\n')
-
-a = {'name':'Yoonghm', 'country':'Singapore'}
-print("a = {'name':'Yoonghm', 'country':'Singapore'}")
-print('type(a) =', type(a), '\n')
+a = None;                         show(a)
+a = True;                         show(a)
+a = 1;                            show(a)
+a = 1234567890;                   show(a)
+a = 12345678901234567890;         show(a)
+a = 0.1;                          show(a)
+a = float_info.max;               show(a)
+a = 1 + 2j;                       show(a)
+a = 'a';                          show(a)
+a = 'ab';                         show(a)
+a = 'abc';                        show(a)
+a = (1);                          show(a)
+a = (1,);                         show(a)
+a = (1, 2);                       show(a)
+a = (1, 2, 3);                    show(a)
+a = ("a", "b");                   show(a)
+a = (1, "a");                     show(a)
+a = (1, (1,2));                   show(a)
+a = [1];                          show(a)
+a = [1,2];                        show(a)
+a = {'x':1};                      show(a)
+a = {'x':1, 'y':2};               show(a)
+a = {'x':1, 'y':2, 'z':3};        show(a)
+a = set(["Access"]);              show(a)
+a = set(["a", "b", "c", "a"]);    show(a)
+a = set(("a", "b", "c", "a"));    show(a)
