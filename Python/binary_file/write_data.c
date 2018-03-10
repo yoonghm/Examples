8375444d38b5b9dc82efe56705a8292a54f987fa
YoongHM <yoonghm@users.noreply.github.com>
2018-03-11 01:20:29 +0800

    Create write_data.c

---
+++ Python/binary_file/write_data.c
@@ -0,0 +1,55 @@
+/* write_data.c */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+
+#include "data_struct.h"
+
+tData db[] = {
+        {1, "Hong Hua", 2000.0},
+        {2, "Ali Mohd", 2000.0},
+        {3, "Ravi Abu", 2200.0}
+};
+
+int main()
+{
+  int      fd;     /* File number                                     */
+  size_t   ReSz;   /* Size of a tData record                          */
+  size_t   n;      /* Number of records in db                         */
+
+  /*****************************/
+  /* Create and open data file */
+  /*****************************/
+  fd = open("./database.db", O_RDWR|O_CREAT|O_TRUNC, 0644);
+  if (fd<0) {
+    perror("open(): ");
+    close(fd);
+    return(-1);
+  }
+
+  /************************/
+  /* Write data into file */
+  /************************/
+  ReSz = sizeof(tData);
+  n = sizeof(db)/ReSz;
+
+  for (int i=0; i<n; i++) {
+    ssize_t ret = write(fd, (const char *) &db[i], ReSz);
+    if (ret == -1) {
+      perror("write(): ");
+      close(fd);
+      return(-1);
+    }
+  }
+
+  /******************/
+  /* Close the file */
+  /******************/
+  close(fd);
+
+  return 0;
+}
