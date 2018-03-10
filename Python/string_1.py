0024a5c62f3e94349e5efea3edf201164fe3f551
YoongHM <yoonghm@users.noreply.github.com>
2018-03-10 16:26:28 +0800

    Create string_1.py

---
+++ Python/string_1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+str = "Hello World!"
+print(str, "has", len(str), "characters.")
+print(str, "occupies", len(str.encode('utf8')), "bytes.")
+print()
+
+str = 'It says, "How are you today?"'
+print(str)
+print()
+
+str = '''
+This is a very long text and spans several lines.
+This is another line.
+And, this is another line.
+'''
+print(str)
+print()
+
+# Unicode
+# You may need to change the font that supports utf8
+str = "世界你好"
+print(str, "has", len(str), "characters.")
+print(str, "occupies", len(str.encode('utf8')), "bytes.")
+
