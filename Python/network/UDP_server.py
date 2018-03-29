a5d6d4a1722fce6bfcc882e20d58c370c7bc19b1
YoongHM <yoonghm@users.noreply.github.com>
2018-03-29 15:05:20 +0800

    Create UDP_server.py

---
+++ Python/network/UDP_server.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+## Description
+# Listen and display first connection to port=port from any address
+#
+
+import socket
+
+port = 12345
+
+# Create UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Listen to all address to port=port
+sock.bind(('', port))
+
+# Wait for the first connection to buffer of 1024 bytes
+bytes, address,  = s.recvfrom(1024)
+
+print('Received {0} from {1}:{2}'.format(bytes, address[0], address[1]))
