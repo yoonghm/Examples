04fc01d51144585f395585de6d92c4876754b675
YoongHM <yoonghm@users.noreply.github.com>
2018-03-29 15:07:08 +0800

    Create UDP_client.py

---
+++ Python/network/UDP_client.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+## Description
+# Send a broadcast message to port=port in its current network
+#
+import socket
+import ipaddress
+
+# Broadcast address is obtained by combining:
+# network address part and inverse of netmask
+# E.g. IP = 192.168.56.1 Netmask = 255.255.255.0
+# Then Network   address = 192.168.56.  0
+#    Inversed of netmask =   0.  0. 0.255
+#      Broadcast address = 192.168.56.255
+# Use ipaddress standard library to do the work
+
+port      = 12345
+netmask   = '255.255.255.0'
+
+hostname  = socket.gethostname()
+ip        = socket.gethostbyname(hostname)
+# ip can also be obtained using ipaddress.IPv4Address()
+
+net       = ipaddress.IPv4Network(ip + '/' + netmask, False)
+broadcast = str(net.broadcast_address)
+
+print('hostname   : {}'.format(hostname))
+print('ip address : {}'.format(ip))
+print('broadcast  : {}'.format(broadcast))
+
+# Create UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Set it to broadcast at SOL_SOCKET level to 1
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+# Send byte of strings to destination
+sock.sendto(b'Hello world', (broadcast, port))
