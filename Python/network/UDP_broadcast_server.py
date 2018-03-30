a309d6045d5f5e903432b4fc94ae9ca0d9d3e284
YoongHM <yoonghm@users.noreply.github.com>
2018-03-31 02:54:31 +0800

    Update UDP_broadcast_server.py

--- Python/network/UDP_broadcast_server.py
+++ Python/network/UDP_broadcast_server.py
@@ -11,14 +11,13 @@ port = 12345
 # Create UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Listen to all address to port=port
+# Listen to port `port` at all addresses
 sock.bind(('', port))
 
 # Wait for the first connection to buffer of 1024 bytes
-bytes, address,  = sock.recvfrom(1024)
+bytes, address = sock.recvfrom(1024)
 
 print('Received {0} from {1}:{2}'.format(bytes, address[0], address[1]))
 
 # Close socket
 sock.close()
-
