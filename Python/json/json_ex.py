4d02eb6a0048044794673c7c234636e907dd1479
YoongHM <yoonghm@users.noreply.github.com>
2018-05-19 11:39:41 +0800

    Update json_ex.py

--- Python/json/json_ex.py
+++ Python/json/json_ex.py
@@ -4,6 +4,22 @@ data = '{"one" : "1", "two" : "2", "three" : "3"}'
 j = json.loads(data)
 print(j['two'])
 
+
+import json
+
+class Student:
+  def __init__(self, id, name, password):
+    self.id = id
+    self.name = name
+    self.password = password
+
+stud1 = Student(1,'Tan Ah Kau','123456')
+
+# Convert Python object to JSON object
+json1 = json.dumps(stud1.__dict__, separators=(',', ':'))
+print(json)
+
 '''
 2
+{"password":"123456","id":1,"name":"Tan Ah Kau"}
 '''
