52448db5490229d490101acd3e888cc1342e9e94
YoongHM <yoonghm@users.noreply.github.com>
2018-05-19 14:39:32 +0800

    Update json_ex.py

--- Python/json/json_ex.py
+++ Python/json/json_ex.py
@@ -4,9 +4,6 @@ data = '{"one" : "1", "two" : "2", "three" : "3"}'
 j = json.loads(data)
 print(j['two'])
 
-
-import json
-
 class Student:
   def __init__(self, id, name, password):
     self.id = id
